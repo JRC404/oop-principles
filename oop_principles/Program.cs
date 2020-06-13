4938707ee50537e48a8680a28cbceb10d84824e0
jrc404 <jacob.reilly.cooper@gmail.com>
2020-06-13 23:39:37 +0100

    readme created. Program.cs now with 4 methods.

--- oop_principles/Program.cs
+++ oop_principles/Program.cs
@@ -2,11 +2,48 @@
 
 namespace oop_principles
 {
+    public class Mathematics
+    {
+        /*
+            We have created four methods here. Addtion, Subtraction, Multiplication and Division. We have used integers, whole numbers to do this... 
+            We have passed them through as parameters. We will need to modify these to make it work properly.
+        */
+        public int Addition(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public int Subtraction(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+
+        public int Multiplication(int numberOne, int numberTwo) 
+        {
+            return numberOne * numberTwo;
+        }
+
+        // our first look at data types with C#. Why is this incorrect?
+        public int Division(int numberOne, int numberTwo) 
+        {
+            return numberOne / numberTwo;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Mathematics maths = new Mathematics();
+            int addMe = maths.Addition(15, 10);
+            int subtractMe = maths.Subtraction(15, 10);
+            int multiplyMe = maths.Multiplication(15, 10);
+            int divideMe = maths.Division(15, 10);
+            Console.WriteLine(addMe);
+            Console.WriteLine(subtractMe);
+            Console.WriteLine(multiplyMe);
+            Console.WriteLine(divideMe);
+            Console.ReadLine();
         }
     }
 }
